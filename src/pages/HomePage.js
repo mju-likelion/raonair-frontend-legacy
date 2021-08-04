6d7c57fe0071465cf528976d75c2b4ba6581df77
skullKim <space1878@naver.com>
2021-08-04 21:37:02 +0900

    추가: 배경 이미지 추가 완료

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,17 @@
+import styled from 'styled-components';
 
+
+const Background = styled.div`
+  height: 100vh;
+  background: url(${process.env.PUBLIC_URL + '/mainPageBackground.png'}) no-repeat;
+  background-size: 100% 658px ;
+`;
 
 const HomePage = () => {
     return (
         <>
+            <Background>
+            </Background>
         </>
     );
 
