f93e7a7a455397d90048a57786c3bbfb9ab00b37
skullKim <space1878@naver.com>
2021-08-05 14:01:16 +0900

    추가: 선택괸 검색 타겟 하이라이트

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,20 +1,87 @@
-import styled from 'styled-components';
-
+import {useState} from 'react';
+import styled, {css} from 'styled-components';
 
 const Background = styled.div`
   height: 100vh;
+  width: 100%;
   background: url(${process.env.PUBLIC_URL + '/mainPageBackground.png'}) no-repeat;
   background-size: 100% 658px ;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const HeadLine = styled.h2`
+  color: white;
+  margin-top: 100px;
+`;
+
+const SearchTargetBox = styled.div`
+  width: 224px;
+  height: 48px;
+  background: #E5E5E5;
+  border-radius: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+const SearchTarget = styled.p`
+  width: 34px;
+  height: 20px;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 18px;
+  line-height: 20px;
+  /* identical to box height, or 111% */
+  text-align: center;
+
+  /* Font default color */
+  color: #222222;
+
+`;
+
+const HighlightBox = styled.div`
+  width: 106px;
+  height: 36px;
+  background: #E5E5E5;
+  border-radius: 36px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HighlightBoxPlay = styled(HighlightBox)`
+    ${({highlight}) =>
+    (highlight === '연극') && css`
+            background-color: white;
+        `}
+`;
+
+const HighlightBoxTroupe = styled(HighlightBox)`
+    ${({highlight}) =>
+    (highlight === '극단') && css`
+            background-color: white;
+        `};
 `;
 
 const HomePage = () => {
+    const [selectedTarget, setSelectedTarget] = useState('연극');
     return (
         <>
             <Background>
+                <HeadLine>라온에어에 오신걸 환영합니다.  원하시는 연극 또는 극단을 검색해 주세요.</HeadLine>
+                <SearchTargetBox>
+                    <HighlightBoxPlay highlight={selectedTarget}>
+                        <SearchTarget >연극</SearchTarget>
+                    </HighlightBoxPlay>
+                    <HighlightBoxTroupe highlight={selectedTarget}>
+                        <SearchTarget>극단</SearchTarget>
+                    </HighlightBoxTroupe>
+                </SearchTargetBox>
             </Background>
         </>
     );
-
 }
 
 export default HomePage;
