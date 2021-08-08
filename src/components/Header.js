766ab97e815143234d54adcd3e49032c4ec85801
skullKim <kyk990328@gmail.com>
2021-08-08 16:19:07 +0900

    추가: 버튼이 위치할 박스 완성 빨간 배경은 임시로 위치 표시를 위해 사용

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,22 @@ const HeaderBox = styled.div`
   height: 64px;
   width: 100%;
   background-color: gray;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const HeaderLogo = styled.img`
   height: 34px;
   width: 102.34px;
+  margin-left: 48px;
+`;
+
+const ButtonBox = styled.div`
+  height: 36px;
+  width: 154px;
+  margin-right: 48px;
+  background-color: red;
 `;
 
 const Header = () => {
@@ -16,6 +27,7 @@ const Header = () => {
     <>
       <HeaderBox>
         <HeaderLogo src='/logo/HorizontalLogo(light).svg' />
+        <ButtonBox />
       </HeaderBox>
     </>
   );
