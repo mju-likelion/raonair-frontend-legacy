601ae33be6cc4130e34ec1f539392627d41c6b13
skullKim <kyk990328@gmail.com>
2021-08-08 20:54:58 +0900

    추가: 헤더 버튼 데모 완료

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 const HeaderBox = styled.div`
   height: 64px;
   width: 100%;
-  background-color: gray;
+  //background-color: gray;
   display: flex;
   justify-content: space-between;
   align-items: center;
+  border-bottom: 1px solid gray;
 `;
 
 const HeaderLogo = styled.img`
@@ -19,7 +20,19 @@ const ButtonBox = styled.div`
   height: 36px;
   width: 154px;
   margin-right: 48px;
-  background-color: red;
+  //border: 1px solid red;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const HeaderButton = styled.div`
+  height: 36px;
+  width: 73px;
+  //border: 1px solid red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Header = () => {
@@ -27,7 +40,10 @@ const Header = () => {
     <>
       <HeaderBox>
         <HeaderLogo src='/logo/HorizontalLogo(light).svg' />
-        <ButtonBox />
+        <ButtonBox>
+          <HeaderButton>로그인</HeaderButton>
+          <HeaderButton>회원 가입</HeaderButton>
+        </ButtonBox>
       </HeaderBox>
     </>
   );
