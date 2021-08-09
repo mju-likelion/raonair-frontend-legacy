9723a596bf4d3bdcfb4e246754e4bd02c20b2a85
skullKim <kyk990328@gmail.com>
2021-08-10 03:18:21 +0900

    추가: 로그인 모달 완료

--- src/components/modal/LoginModal.js
+++ src/components/modal/LoginModal.js
@@ -1,19 +1,26 @@
+import { useFormik } from 'formik';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import styled, { css } from 'styled-components';
 
 import ModalBox from './CommonModalBox';
 
 const LoginBox = styled(ModalBox)`
-  height: 313px;
-  width: 440px;
+  height: 307px;
+  width: 434px;
   margin-top: -156.5px;
   margin-left: -220px;
-  background-color: gray;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border: 3px solid #49b0ff;
+  border-radius: 12px;
+  background-color: #f6fbff;
 `;
 
 const CloseBox = styled.div`
   width: 440px;
-  height: 15px;
+  height: 9px;
   display: flex;
   flex-direction: row-reverse;
 `;
@@ -21,17 +28,76 @@ const CloseBox = styled.div`
 const CloseSvg = styled.img`
   width: 15px;
   height: 15px;
-  margin-top: 28px;
+  margin-top: 25px;
   margin-right: 33px;
 `;
 
+const ModalTitle = styled.h4`
+  font-weight: normal;
+  margin-bottom: 0px;
+`;
+
+const LoginFormBox = styled.form`
+  height: 149px;
+  width: 337px;
+  margin-top: 26px;
+`;
+
+const commonFromStyle = css`
+  height: 32px;
+  width: 332px;
+  border: 1px solid #49b0ff;
+  border-radius: 12px;
+`;
+
+const LoginInput = styled.input`
+  ${commonFromStyle};
+  text-align: center;
+`;
+
+const LoginPassword = styled(LoginInput)`
+  margin-top: 14px;
+`;
+
+const LoginSubmit = styled.button`
+  ${commonFromStyle};
+  height: 34px;
+  width: 335px;
+  margin-top: 30px;
+  background-color: #49b0ff;
+  color: white;
+  font-size: 16px;
+`;
+
+const FindPasswordBtn = styled(Link)`
+  margin-top: 32px;
+  font-size: 12px;
+`;
+
 const LoginModal = ({ onClose }) => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+    onSubmit: () => {},
+  });
+
   return (
     <>
       <LoginBox>
         <CloseBox>
           <CloseSvg src='/svg/close.svg' onClick={onClose} />
         </CloseBox>
+        <ModalTitle>로그인</ModalTitle>
+        <LoginFormBox onSubmit={formik.handleSubmit}>
+          <LoginInput placeholder='이메일 아이디' />
+          <LoginPassword type='password' placeholder='비밀번호' />
+          <LoginSubmit>로그인</LoginSubmit>
+        </LoginFormBox>
+        <FindPasswordBtn to='/auth/password-find' onClick={onClose}>
+          비밀번호 찾기
+        </FindPasswordBtn>
       </LoginBox>
     </>
   );
