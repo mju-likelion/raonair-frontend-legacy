3358e44bb395bdeae760a743a25f5b38765da008
skullKim <kyk990328@gmail.com>
2021-08-10 04:45:41 +0900

    추가: 회원가입 모달 디자인 완료

--- src/components/modal/CommonModalBox.js
+++ src/components/modal/CommonModalBox.js
@@ -4,7 +4,7 @@ const ModalBox = styled.div`
   position: absolute;
   left: 50%;
   top: 50%;
-  border: 3px solid #49b0ff;
+  border: 2px solid #49b0ff;
   border-radius: 12px;
   background-color: #f6fbff;
 `;
--- src/components/modal/signupModal.js
+++ src/components/modal/signupModal.js
@@ -1,13 +1,12 @@
 import { useFormik } from 'formik';
 import PropTypes from 'prop-types';
-// import styled, { css } from 'styled-components';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import ModalBox from './CommonModalBox';
 
 const SignupBox = styled(ModalBox)`
-  height: 391px;
-  width: 438px;
+  height: 393px;
+  width: 440px;
   margin-top: -198.5px;
   margin-left: -222px;
   display: flex;
@@ -25,8 +24,8 @@ const CloseBox = styled.div`
 const CloseSvg = styled.img`
   width: 15px;
   height: 15px;
-  margin-top: 28px;
-  margin-right: 37px;
+  margin-top: 24px;
+  margin-right: 35px;
 `;
 
 const ModalTitle = styled.h4`
@@ -34,18 +33,36 @@ const ModalTitle = styled.h4`
   margin-bottom: 0;
 `;
 
-// const commonFormStyle = css`
-//   height: 35px;
-//   width: 340px;
-//   border: 1px solid #49b0ff;
-//   border-radius: 12px;
-// `;
-
 const SignupFormBox = styled.form`
   height: 248px;
   width: 340px;
+  margin-top: 40px;
+`;
+
+const commonFormStyle = css`
+  height: 33px;
+  width: 333px;
+  border: 1px solid #49b0ff;
+  border-radius: 12px;
+  font-size: 14px;
+`;
+
+const FirstFormInput = styled.input`
+  ${commonFormStyle};
+  text-align: center;
+`;
+
+const RestFormInput = styled(FirstFormInput)`
+  margin-top: 12px;
+`;
+
+const SignupSubmit = styled.button`
+  ${commonFormStyle};
+  width: 340px;
   margin-top: 31px;
-  background-color: red;
+  background-color: #49b0ff;
+  color: white;
+  font-size: 16px;
 `;
 
 const SignupModal = ({ onClose }) => {
@@ -61,12 +78,18 @@ const SignupModal = ({ onClose }) => {
 
   return (
     <>
-      <SignupBox onClick={onClose}>
-        <CloseBox>
+      <SignupBox>
+        <CloseBox onClick={onClose}>
           <CloseSvg src='/svg/close.svg' />
         </CloseBox>
         <ModalTitle>회원가입</ModalTitle>
-        <SignupFormBox onSubmit={formik.handleSubmit} />
+        <SignupFormBox onSubmit={formik.handleSubmit}>
+          <FirstFormInput placeholder='이메일' />
+          <RestFormInput type='password' placeholder='비밀번호' />
+          <RestFormInput placeholder='이름' />
+          <RestFormInput placeholder='닉네임' />
+          <SignupSubmit>회원가입</SignupSubmit>
+        </SignupFormBox>
       </SignupBox>
     </>
   );
