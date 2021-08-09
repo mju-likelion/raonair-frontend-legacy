8a6aecb8784410b3413f9471fb7c5965933f168d
skullKim <kyk990328@gmail.com>
2021-08-10 00:36:19 +0900

    수정: GlobalStyles 코드 수정 구글 폰트 가져오는 부분 index.html로 이동

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300&display=swap');
   html, body {
     height: 100%;
     margin: 0;
