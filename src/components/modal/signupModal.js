778925e496405de0e938c33c81a720ecd7d97dac
skullKim <kyk990328@gmail.com>
2021-08-10 04:07:53 +0900

    추가: 회원 가입 모달 일부 완료 모달 제목, 모달 끄기 버튼 회원가입 정보 입력 부분 골자 위치 완료

--- src/components/modal/signupModal.js
+++ src/components/modal/signupModal.js
@@ -1,4 +1,6 @@
+import { useFormik } from 'formik';
 import PropTypes from 'prop-types';
+// import styled, { css } from 'styled-components';
 import styled from 'styled-components';
 
 import ModalBox from './CommonModalBox';
@@ -8,12 +10,64 @@ const SignupBox = styled(ModalBox)`
   width: 438px;
   margin-top: -198.5px;
   margin-left: -222px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const CloseBox = styled.div`
+  width: 438px;
+  height: 9px;
+  display: flex;
+  flex-direction: row-reverse;
+`;
+
+const CloseSvg = styled.img`
+  width: 15px;
+  height: 15px;
+  margin-top: 28px;
+  margin-right: 37px;
+`;
+
+const ModalTitle = styled.h4`
+  font-weight: normal;
+  margin-bottom: 0;
+`;
+
+// const commonFormStyle = css`
+//   height: 35px;
+//   width: 340px;
+//   border: 1px solid #49b0ff;
+//   border-radius: 12px;
+// `;
+
+const SignupFormBox = styled.form`
+  height: 248px;
+  width: 340px;
+  margin-top: 31px;
+  background-color: red;
 `;
 
 const SignupModal = ({ onClose }) => {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      name: '',
+      nickName: '',
+    },
+    onSubmit: () => {},
+  });
+
   return (
     <>
-      <SignupBox onClick={onClose} />
+      <SignupBox onClick={onClose}>
+        <CloseBox>
+          <CloseSvg src='/svg/close.svg' />
+        </CloseBox>
+        <ModalTitle>회원가입</ModalTitle>
+        <SignupFormBox onSubmit={formik.handleSubmit} />
+      </SignupBox>
     </>
   );
 };
