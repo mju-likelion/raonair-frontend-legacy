9e04ffffb32c2ea82f27d7f072bf2129b30cc8fe
skullKim <kyk990328@gmail.com>
2021-08-12 00:49:45 +0900

    추가: 검색 페이지 항목별 뼈대 완료

--- src/pages/SearchResultPage.js
+++ src/pages/SearchResultPage.js
@@ -1,5 +1,26 @@
-function SearchResultPage() {
-  return <>SearchResultPage</>;
-}
+import styled from 'styled-components';
+
+const PlaysBox = styled.div`
+  height: 477px;
+  width: 1182px;
+  margin-bottom: 50px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: red;
+`;
+
+const FirstPlaysBox = styled(PlaysBox)`
+  margin-top: 81px;
+`;
+
+const SearchResultPage = () => {
+  return (
+    <>
+      <FirstPlaysBox />
+      <PlaysBox />
+      <PlaysBox />
+    </>
+  );
+};
 
 export default SearchResultPage;
