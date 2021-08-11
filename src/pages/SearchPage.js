051b50e7f3cebdc9d9b177bc807d436a2bcc5745
skullKim <kyk990328@gmail.com>
2021-08-12 01:21:14 +0900

    수정: 잘못된 페이지에서 하던 코딩 원위치 SearchPage.js에서 해야 되는걸 SearchResultPage.js에서 하고 있었다..

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,7 +1,54 @@
+import { NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+
+const PlaysBox = styled.div`
+  height: 477px;
+  width: 1182px;
+  margin-bottom: 50px;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: red;
+`;
+
+const FirstPlaysBox = styled(PlaysBox)`
+  margin-top: 81px;
+`;
+
+const PlayBoxNav = styled.div`
+  width: 1182px;
+  height: 25px;
+  background-color: gray;
+`;
+
+const BoxTitle = styled.h4`
+  font-size: 20px;
+  font-weight: normal;
+`;
+
+const ShowMoreBtn = styled(NavLink)`
+  font-size: 20px;
+  color: #49b0ff;
+`;
+
 function SearchPage() {
   // query string 있으면 검색결과 렌더링
   // 없으면 검색 창을 렌더링
-  return <>Search</>;
+  return (
+    <>
+      <FirstPlaysBox>
+        <PlayBoxNav>
+          <BoxTitle>진행중인 공연</BoxTitle>
+          <ShowMoreBtn to='search/play/on-going'>더보기</ShowMoreBtn>
+        </PlayBoxNav>
+      </FirstPlaysBox>
+      <PlaysBox>
+        <BoxTitle>상영 예정인 공연</BoxTitle>
+      </PlaysBox>
+      <PlaysBox>
+        <BoxTitle>종료된 공연</BoxTitle>
+      </PlaysBox>
+    </>
+  );
 }
 
 export default SearchPage;
--- src/pages/SearchResultPage.js
+++ src/pages/SearchResultPage.js
@@ -1,37 +1,5 @@
-import styled from 'styled-components';
-
-const PlaysBox = styled.div`
-  height: 477px;
-  width: 1182px;
-  margin-bottom: 50px;
-  margin-left: auto;
-  margin-right: auto;
-  background-color: red;
-`;
-
-const FirstPlaysBox = styled(PlaysBox)`
-  margin-top: 81px;
-`;
-
-const BoxTitle = styled.h4`
-  font-size: 20px;
-  font-weight: normal;
-`;
-
 const SearchResultPage = () => {
-  return (
-    <>
-      <FirstPlaysBox>
-        <BoxTitle>진행중인 공연</BoxTitle>
-      </FirstPlaysBox>
-      <PlaysBox>
-        <BoxTitle>상영 예정인 공연</BoxTitle>
-      </PlaysBox>
-      <PlaysBox>
-        <BoxTitle>종료된 공연</BoxTitle>
-      </PlaysBox>
-    </>
-  );
+  return <div>검색 종합 페이지</div>;
 };
 
 export default SearchResultPage;
