36d87f313c0a6015179a70964905c0cbf673df61
skullKim <kyk990328@gmail.com>
2021-08-12 02:31:55 +0900

    추가: 단일 연극 표시하는 박스 골자 완료

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -36,6 +36,53 @@ const ShowMoreBtn = styled(NavLink)`
   color: #49b0ff;
 `;
 
+const PlayBox = styled.div`
+  height: 420px;
+  width: 240px;
+  margin-top: 32px;
+  background-color: blue;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const JudgeBox = styled.div`
+  height: 28px;
+  width: 213px;
+  display: flex;
+  align-items: center;
+  background-color: green;
+`;
+
+const Judge = styled.div`
+  height: 28px;
+  width: 58px;
+  display: flex;
+  align-items: center;
+`;
+
+const JudgeImg = styled.img`
+  height: 20px;
+  width: 21.86px;
+`;
+
+const PlayImage = styled.img`
+  height: 299px;
+  width: 213px;
+`;
+
+const PlayTitle = styled.h4`
+  font-size: 20px;
+  margin: 0;
+`;
+
+const PlayDate = styled.p`
+  font-size: 18px;
+  font-weight: lighter;
+  margin: 0;
+`;
+
 function SearchPage() {
   // query string 있으면 검색결과 렌더링
   // 없으면 검색 창을 렌더링
@@ -53,6 +100,21 @@ function SearchPage() {
             더보기
           </ShowMoreBtn>
         </PlayBoxNav>
+        <PlayBox>
+          <JudgeBox>
+            <Judge>
+              <JudgeImg src='/svg/star.svg' alt='평점' />
+              4.5
+            </Judge>
+            <Judge>
+              <JudgeImg src='/svg/heart.svg' alt='찜 갯수' />
+              99
+            </Judge>
+          </JudgeBox>
+          <PlayImage />
+          <PlayTitle>특별한 저녁식사</PlayTitle>
+          <PlayDate>9999.99.99~9999.99.99</PlayDate>
+        </PlayBox>
       </FirstPlaysBox>
       <PlaysBox>
         <PlayBoxNav>
