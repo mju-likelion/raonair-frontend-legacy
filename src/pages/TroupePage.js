beb297d2bd6d86a3dee485826d64591861ebc13b
Bhanjo <hanjo1515@naver.com>
2021-08-13 13:40:50 +0900

    추가: 개별 극단 api 호출

--- src/pages/TroupePage.js
+++ src/pages/TroupePage.js
@@ -1,5 +1,36 @@
-function TroupePage() {
-  return <>TroupePage</>;
-}
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+const TroupePage = () => {
+  const [troupe, setTroupe] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/troupe/1');
+        setTroupe(response.data.data);
+      } catch (e) {
+        // console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <h1>로딩중</h1>;
+  }
+  if (!troupe) {
+    return null;
+  }
+
+  return (
+    <>
+      {troupe && <textarea rows={7} value={JSON.stringify(troupe, null, 2)} />}
+    </>
+  );
+};
 
 export default TroupePage;
