2441ef109cb06e9c7784b9734370e49f7757ba3e
skullKim <kyk990328@gmail.com>
2021-08-13 20:54:26 +0900

    추가: 더보기 검색에서 데이터 가저오기 완료 일단은 임시 데이터로 테스트

--- src/pages/SearchResultPage.js
+++ src/pages/SearchResultPage.js
@@ -1,5 +1,35 @@
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
 const SearchResultPage = () => {
-  return <div>검색 종합 페이지</div>;
+  const [reqCondition, setReqCondition] = useState({
+    query: '째',
+    start: 1,
+    limit: 20,
+    type: 'on-going',
+    url: `${process.env.REACT_APP_SERVER_ORIGIN}/api/search/play/on-going`
+  });
+  const [searchedData, setSearchedData] = useState([]);
+  useEffect(() => {
+    axios({
+      method: 'get',
+      url: `${reqCondition.url}`,
+      params: {
+        query: `${reqCondition.query}`,
+        start: reqCondition.start,
+        limit: reqCondition.limit,
+      }
+    })
+      .then(({data: { data: { search_results: searchResults }, links: {next: nextUrl} }}) => {
+        const {start, limit} = reqCondition;
+        setReqCondition({...reqCondition, start: start + limit, url: `${process.env.REACT_APP_SERVER_ORIGIN+nextUrl}`});
+        setSearchedData(searchResults);
+      })
+      .catch(err => {
+        return err;
+      })
+  })
+  return <div>a</div>
 };
 
 export default SearchResultPage;
