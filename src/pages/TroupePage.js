313498f478f3a88ad1e5916bf000144f5a7fa187
Bhanjo <hanjo1515@naver.com>
2021-08-13 20:35:27 +0900

    Revert "추가: 극단 구성원 정보 추가"

--- src/pages/TroupePage.js
+++ src/pages/TroupePage.js
@@ -13,6 +13,13 @@ const TitleBox = styled.div`
   }
   .titleButtons {
     margin-right: 50px;
+    img {
+      float: right;
+      cursor: pointer;
+    }
+    img:hover {
+      background-image: url('/svg/like_on.svg');
+    }
     p {
       font-weight: bold;
       cursor: pointer;
@@ -35,12 +42,59 @@ const TitleBox = styled.div`
   }
 `;
 
+const StaffBox = styled.div`
+  width: 1150px;
+  height: 276px;
+  margin: 0 auto;
+  font-weight: bold;
+  .staffHeader {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    p {
+      font-size: 36px;
+    }
+  }
+  .staffHeader::before,
+  .staffHeader::after {
+    content: '';
+    background: #529acc;
+    width: 150px;
+    height: 5px;
+    font-size: 0px;
+    line-height: 0px;
+    margin: 0 19px;
+  }
+  .staffList {
+    width: 1015px;
+    height: 193px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    /* 오버플로우에 따른 슬라이드 구현하기 */
+    overflow-x: scroll;
+    p {
+      text-align: center;
+    }
+  }
+  .staffItem {
+    margin: 0 67px;
+    img {
+      width: 120px;
+      height: 120px;
+      border-radius: 100%;
+      background: #f2f2f2;
+    }
+  }
+`;
 // 로고에 따른 스타일 변경 필요
-const TroupePage = id => {
+// 좋아요 기능 구현 필요
+const TroupePage = () => {
   const [troupe, setTroupe] = useState(null);
   const [loading, setLoading] = useState(true);
+  // const [likeImg, setLikeImg] = useState('/svg/like_off.svg');
   // 임시 극단 id값, 추후 props에서 받아온 값으로 수정해야함
-  const troupeId = 2;
+  const troupeId = 1;
 
   // api 호출
   useEffect(() => {
@@ -57,14 +111,20 @@ const TroupePage = id => {
       setLoading(false);
     };
     fetchData();
-  }, [id]);
+  }, []);
 
   if (loading) {
-    return <h1>로딩중</h1>;
+    return <h1>loading...</h1>;
   }
   if (!troupe) {
     return null;
   }
+  // 좋아요 여부 판단
+  // if (troupe.context.like_check) {
+  //   setLikeImg('/svg/like_on.svg');
+  // } else {
+  //   setLikeImg('/svg/like_off.svg');
+  // }
 
   return (
     <>
@@ -76,14 +136,25 @@ const TroupePage = id => {
           <p>{troupe.troupe.name}</p>
         </div>
         <div className='titleButtons'>
-          {/*
-            클릭하면 정보 수정페이지로 넘어가게 현재 미구현
-            찜하기 버튼은 이미지로 수정 필요
-           */}
           <p>잘못된 정보가 있나요?</p>
-          <p>찜하기버튼</p>
+          <img src='/svg/like_off.svg' alt='좋아요버튼' />
         </div>
       </TitleBox>
+      <StaffBox>
+        <div className='staffHeader'>
+          <p>극단 구성원</p>
+        </div>
+        {/* <p></p> */}
+        <div className='staffList'>
+          {troupe.team.map(team => (
+            <div className='staffItem' key={team.name}>
+              {/* <img src={team.photo} alt='인물 이미지' /> */}
+              <img src='/svg/people_default.svg' alt='인물 이미지' />
+              <p>{team.name}</p>
+            </div>
+          ))}
+        </div>
+      </StaffBox>
     </>
   );
 };
